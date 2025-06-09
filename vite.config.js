54b07335abe90ce2f2d95158d110f8ef2e9c3be9
Natalia Jordan <taliandre49@gmail.com>
2025-06-09 15:16:09 -0400

    updating build configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { BootstrapVueNextResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.DEPLOY === "github" ? '/ArtworkCatalog/' : './',
+  base: '/',
   plugins: [
     vue(),
     VitePWA({
